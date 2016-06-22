b407e64716c46979a658d9ce92c9347d4c82f83c
juliajau <juliajau@DE-JULIAJAU01>
2016-06-22 09:22:32 +0200

    Projektdateien hinzufügen.

---
+++ KuduDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KuduDemo.Startup))]
+namespace KuduDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
